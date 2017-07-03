09e8c8cc8c90b086030ec657e091400cad680690
lsfnts <buho9626@gmail.com>
2017-07-02 23:25:24 -0600

    aqui va la exception

---
+++ src/analisis/math/InvalidInput.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package analisis.math;
+
+/**
+ *
+ * @author Luis
+ */
+public class InvalidInput extends Exception{
+	public InvalidInput(String message) {
+        super(message);
+    }
+}
